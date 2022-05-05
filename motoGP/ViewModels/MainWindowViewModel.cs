b5c039f278b199fa6be6b5b4d56ade73938a983c
Илья Харченко <kharchenko_ilia@mail.ru>
2022-05-06 03:13:41 +0700

    The interface has been developed

--- motoGP/ViewModels/MainWindowViewModel.cs
+++ motoGP/ViewModels/MainWindowViewModel.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using System.Collections.ObjectModel;
 using motoGP.Models;
+using ReactiveUI;
+using System.Reactive;
 
 namespace motoGP.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+
         public ObservableCollection<DefaultObj> Items { get; set; }
         //new DefaultObj(1, "a", "b"), new DefaultObj(2, "c", "d"), new DefaultObj(3, "e", "f")
         public MainWindowViewModel()
